2c53361526580ec86881b6d061024f273a31bf26
MSI <serejaskok@mail.ru>
2023-02-20 16:24:47 +0300

    update

--- script/script.js
+++ script/script.js
@@ -4,22 +4,23 @@ let closeButton = document.querySelector(".popup__vector");
 let form = document.querySelector(".form");
 let nameForm = document.querySelector(".profile__title");
 let about = document.querySelector(".profile__subtitle");
-let formdata = document.querySelectorAll(".form__data");
+let formName = document.querySelector(".form__data_type_name");
+let formAbout = document.querySelector(".form__data_type_about");
 
 function popupAdd() {
   popup.classList.add("popup_opened");
+  formName.value = nameForm.textContent;
+  formAbout.value = about.textContent;
 }
 function popupClose() {
   popup.classList.remove("popup_opened");
 }
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  nameForm.textContent = formdata[0].value;
-  about.textContent = formdata[1].value;
+  nameForm.textContent = formName.value;
+  about.textContent = formAbout.value;
   popupClose();
 }
-formdata[0].value = nameForm.textContent;
-formdata[1].value = about.textContent;
 
 editbutton.addEventListener("click", popupAdd);
 
